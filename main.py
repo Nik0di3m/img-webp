07903e3144feb0154e6c92e479859091e45f49fe
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-07-28 11:03:00 +0200

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+import glob
+
+folder_input = 'images'
+
+folder_output = 'converted'
+
+news_img = os.listdir(folder_input)
+
+news_img_select = [file for file in news_img if file.endswith(('jpg', 'png'))]
+
+for n_img in news_img_select:
+    image = Image.open(folder_input + '/' + n_img)
+    image.convert('RGB')
+
+    file = n_img.replace('.jpg', '').replace('.png', '')
+
+    image.save(folder_output + '/' + file + '.webp', 'webp')
+    print('--- Done', n_img)
+
